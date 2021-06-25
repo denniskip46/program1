3b7c427b49d8085c42d351274d2986f32a1d94be
denniskip46 <85351159+denniskip46@users.noreply.github.com>
2021-06-25 01:35:55 -0900

    Add files via upload

---
+++ sebastiano.c
@@ -0,0 +1,24 @@
+//"a program of c operator"
+#include<stdio.h>
+int main()
+{
+int a=7,c=4;
+if(a==c)
+{
+	
+	
+	
+	printf("a and c are equal:");
+}
+
+else
+{
+
+	printf("a and c are not equal");
+
+}
+	
+	
+	
+	return 0;
+}
